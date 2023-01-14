4124d013db1d95913d668796db293f5d30ad8b7a
LoicZHU <zhu.loic@gmail.com>
2023-01-14 20:46:11 +0100

    update Item.ts

--- src/Item.ts
+++ src/Item.ts
@@ -1,39 +1,44 @@
 export default class Item {
 
-  private name : string ; 
-  private creationDate : Date ; 
+  private name : string ;
+  private creationDate : Date ;
 
-  constructor(name : string , date : Date) {
-      this.name = name ; 
-      this.creationDate = date ; 
+  constructor(name : string , private content: string, date : Date) {
+			this.checkContent(content);
+
+      this.name = name ;
+      this.creationDate = date ;
   }
 
 
   getDate () : Date {
-    return this.creationDate ; 
+    return this.creationDate ;
   }
 
   getItemName () : string {
-    return this.name ;  
+    return this.name ;
   }
 
   setItemName (itemName : string) : void {
     this.name =  itemName ;
   }
 
+	getContent(): string {
+		return this.content;
+	}
+
+	setContent(value: string) {
+		this.content = value;
+	}
+
   setItemDate (date : Date) : void {
-    this.creationDate = date ; 
+    this.creationDate = date ;
   }
-  // checking the name max size 
-  checkItemName () : Error | boolean {
 
-    if (this.name.length > 1000 ) {
-      return Error ("max lenght exceeded") ; 
+  checkContent(content: string) : void | Error {
+    if (content.length > 1000) {
+      throw Error ("max lenght exceeded");
     }
-
-    return true ;
   }
-
-  
 
 }
